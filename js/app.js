f9b2d1cd2e6d313026353c51e06e79d5283306d6
Dorin <doriodavid@gmail.com>
2021-01-31 22:43:25 +0100

    random hex color changer

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
-(function() {
-    const button = document.querySelector('#btn')
-    const body = document.querySelector('body')
+
+let textTarget = document.getElementById("hex-value");
+let bodyBackgroundReference = document.body.style;
+let triggerButton = document.getElementById(id="btn");
+
+
+triggerButton.addEventListener('click', (function(){
     const hexValues = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
-    const value = document.querySelector('#hex-value')
-
-    button.addEventListener('click', changeHex)
-
-    function changeHex(){
-        let hex = '#'
-
-        for (let i = 0; i < 6; i++){
-            const index = Math.floor(Math.random()*hexValues.length)
-            hex += hexValues[index]
-        }
-        value.textContent = hex
-        body.style.backgroundColor = hex
+  return function(){
+    let hex = '#'
+    for(let i = 0; i < 6; i++){
+      let index = Math.floor(Math.random() * hexValues.length);
+      hex += hexValues[index]
     }
-} )()
+     bodyBackgroundReference.background = hex;
+     textTarget.textContent = hex;
+     
+  }
+
+}()))
